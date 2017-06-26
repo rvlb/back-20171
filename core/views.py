570ed595f60b7cc6c7902954230f65e1cdc2f546
rvlb-19 <rvlb@cin.ufpe.br>
2017-06-26 09:07:21 -0300

    Add ementa

--- core/views.py
+++ core/views.py
@@ -15,6 +15,17 @@ class IndexView(TemplateView, NavbarMixin):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['name'] = 'Renato'
+        context['content'] = [
+            { 'title': 'Instalação e criação de um projeto', 'status': True },
+            { 'title': 'Model, Template & View', 'status': True },
+            { 'title': 'URLs', 'status': True },
+            { 'title': 'Admin', 'status': True },
+            { 'title': 'Integração Front-end/Back-end', 'status': True },
+            { 'title': 'Template tags', 'status': True },
+            { 'title': 'Forms', 'status': True },
+            { 'title': 'Autenticação (se der tempo)', 'status': True },
+            { 'title': 'Django + AJAX (se der tempo)', 'status': True },
+        ]
         return context
 
 class HobbiesView(ListView, NavbarMixin):
